1bc6c82c54c18f73fdeb164ed52ddf3e8b32077d
DESKTOP-PQ8QHKV\matt <Matthewcpye@hotmail.com>
2019-03-12 01:29:46 +0000

    Added Tutorial 4

---
+++ Tutorial 4/Holiday Check/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holiday_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hoilday");
+            labelDuration:
+            Console.WriteLine("\nDuration of stay? (2, 7 or 14 nights)");
+            var duration = Console.ReadLine();
+
+            switch (duration)
+            {
+                case "2":
+                    break;
+
+                case "7":
+                    break;
+
+                case "14":
+                    break;
+
+                default:
+                    goto labelDuration;
+            }
+
+            labelGuests:
+            Console.WriteLine("\nNumber of guests?");
+            try
+            {
+                int NoGuests = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                goto labelGuests;
+            }
+
+
+            
+
+            labelBoard:
+            Console.WriteLine("\nType of board? (Full, Half or Self-catering)");
+            var board = Console.ReadLine().ToLower();
+
+            switch (board)
+            {
+                case "full":
+                    break;
+
+                case "half":
+                    break;
+
+                case "self-catering":
+                    break;
+
+                default:
+                    goto labelBoard;
+            }
+
+            Console.WriteLine("\n\nInput complete!!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tutorial 4/Post/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LabelMain:
+            Console.WriteLine("Please enter the letter that you have been given");
+
+            var letter = Console.ReadLine();
+            switch (letter)
+            {
+
+                case "b":
+                    Console.WriteLine("\nBills must be paid");
+                    break;
+
+                case "c":
+                    Console.WriteLine("\nCirculars are thrown away");
+                    break;
+
+                case "p":
+                    Console.WriteLine("\nPostcards are put on the wall");
+                    break;
+
+                case "l":
+                    Console.WriteLine("\nPersonal letters are read and have repies written for them");
+                    break;
+
+                default:
+                    Console.Clear();
+                    Console.WriteLine("\nInvaild input");
+                    System.Threading.Thread.Sleep(2000);
+                    Console.Clear();
+                    goto LabelMain;
+            }
+
+        }
+    }
+}
