63434e906a9a76dcc9f2ea99f2b0ccb9add7802d
DESKTOP-PQ8QHKV\matt <Matthewcpye@hotmail.com>
2019-03-12 01:31:41 +0000

    Added Tutorial11

---
+++ Tutorial 11/StudentNames/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace StudentNames
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FirstName = new System.Windows.Forms.TextBox();
+            this.SecondName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Submit = new System.Windows.Forms.Button();
+            this.Quit = new System.Windows.Forms.Button();
+            this.SubmitTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // FirstName
+            // 
+            this.FirstName.Location = new System.Drawing.Point(12, 55);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(151, 20);
+            this.FirstName.TabIndex = 2;
+            // 
+            // SecondName
+            // 
+            this.SecondName.Location = new System.Drawing.Point(12, 121);
+            this.SecondName.Name = "SecondName";
+            this.SecondName.Size = new System.Drawing.Size(151, 20);
+            this.SecondName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Second Name";
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(12, 175);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(75, 23);
+            this.Submit.TabIndex = 5;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // Quit
+            // 
+            this.Quit.Location = new System.Drawing.Point(172, 175);
+            this.Quit.Name = "Quit";
+            this.Quit.Size = new System.Drawing.Size(75, 23);
+            this.Quit.TabIndex = 6;
+            this.Quit.Text = "Quit";
+            this.Quit.UseVisualStyleBackColor = true;
+            this.Quit.Click += new System.EventHandler(this.Quit_Click);
+            // 
+            // SubmitTimer
+            // 
+            this.SubmitTimer.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 222);
+            this.Controls.Add(this.Quit);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.SecondName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FirstName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox FirstName;
+        private System.Windows.Forms.TextBox SecondName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Button Quit;
+        private System.Windows.Forms.Timer SubmitTimer;
+    }
+}
+
---
+++ Tutorial 11/StudentNames/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentNames
+{
+    public partial class Form1 : Form
+    {
+        static FileStream Output = new FileStream("Output.txt", FileMode.Create);
+        StreamWriter swOut = new StreamWriter(Output);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            swOut.WriteLine("{0} \t {1}",FirstName.Text, SecondName.Text);
+            FirstName.Text = "";
+            SecondName.Text = "";
+        }
+
+        private void Quit_Click(object sender, EventArgs e)
+        {
+            Output.Close();
+            Environment.Exit(1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            swOut.WriteLine("First Name \t Second Name");
+        }
+    }
+}
