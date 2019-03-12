1e6850b6abe573e14f5d6ed3e23eee3053638165
DESKTOP-PQ8QHKV\matt <Matthewcpye@hotmail.com>
2019-03-12 01:30:57 +0000

    Added Tutorial 9

---
+++ Tutorial 9/Candidates/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candidates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] Candidates = new string[5] {"Ahmed","Boo","Celine","Didi","Elaine"};
+            int[] Votes = new int[5];
+
+            int input;
+
+            do
+            {
+
+                Console.WriteLine("Which candidate are you voting for: ");
+                input = Convert.ToInt32(Console.ReadLine());
+
+                if (input > 4)
+                {
+                    Console.WriteLine("Invaild input");
+                }
+                else if (input == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    Votes[input]++;
+                }
+            } while (input != -1);
+
+            Console.WriteLine("Vote Summary");
+            Array.Sort(Votes,Candidates,0,5);
+            Console.WriteLine("{0} {1}",Votes[0],Candidates[0]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tutorial 9/LiteraryAuthors/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteraryAuthors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] Authors = new string[10] {"william shakespeare", "mark twain", "jane austin", "charlotte bronte", "lousia may alcott", "lewis carroll", "d.h. lawrence", "charles dickens", "lucy maud montgomery", "alexander dumas"};
+
+            Console.WriteLine("Enter an authors name: ");
+            string input = Console.ReadLine().ToLower();
+            int inputNum = 0;
+
+            do
+            {
+                if (input == Authors[inputNum])
+                {
+                    Console.WriteLine("The author that you have input is placed {0}", inputNum+1);
+                    Console.ReadLine();
+                    break;
+                }
+
+                inputNum++;
+            } while (inputNum != 10);
+
+            if (inputNum >= 10)
+            {
+                Console.WriteLine("The author that you have input is not in the top 10");
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
