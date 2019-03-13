264de5611b8434152b2a0964fe83e1f60fddc68a
Smkautltl <matthewcpye@hotmail.com>
2019-03-13 15:10:43 +0000

    Added Tutorial 1

---
+++ Tutorial 1/PersonalDetails/PersonalDetails/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalDetails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: Matthew Pye");
+            Console.WriteLine("Address: 87 Shewsbury rd, Market Drayton, Shopshire, TF9 3DN");
+            Console.WriteLine("Fact: I can program in 3 lanuages");
+
+            Console.ReadLine();
+        }
+    }
+}
